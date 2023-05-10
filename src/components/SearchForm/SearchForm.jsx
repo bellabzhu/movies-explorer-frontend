d4df9e21e946362829fe85b8e76486c7cb8cb781
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-05-11 00:49:16 +0300

    feat: layout for search input in movies page

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,9 +2,18 @@ import './SearchForm.css';
 
 function SearchForm () {
   return(
-    <>
-
-    </>
+    <div className="search">
+      <div className="search__container">
+        <form className="search__form">
+          <input className="search__input" placeholder="Фильм"></input>
+          <button className="search__submit" type="submit" />
+        </form>
+        <label className="search__label">
+          <input className="search__toggle" type="checkbox" id="toggle" />
+          Короткометражки
+        </label>
+      </div>
+    </div>
   )
 }
 
