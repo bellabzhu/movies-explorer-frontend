26695f5ca6fa8b480de69389a394c5d7e61ba2e5
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-05-11 01:20:46 +0300

    feat hover to about-me link

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -13,7 +13,9 @@ function AboutMe () {
           <h3 className="about-me__name">Белла</h3>
           <p className="about-me__job">Фронтенд-разработчица, 29&nbsp;лет</p>
           <p className="about-me__description">Фронтенд-разработчица с бесконечным терпением, готовая тыкать в пиксели до тех пор, пока они не станут идеально круглыми. Я не придумала оригинального описания, поэтому скопирую остальное с макета. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «Awesome». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-          <Link className="about-me__link" to="https://github.com/bellabzhu">GitHub</Link>
+          <div className="about-me__link-container">
+            <Link className="about-me__link" to="https://github.com/bellabzhu">GitHub</Link>
+          </div>
           <img className="about-me__pic" src={avatar} alt="Иллюстрация девушки со светлыми волосами в фэнтази стиле."></img>
         </div>
         <Portfolio />
